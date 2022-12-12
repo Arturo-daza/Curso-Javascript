ff1b326575febb1cf8ea3652044258cb659db953
Arturo-daza <118765168+Arturo-daza@users.noreply.github.com>
2022-12-12 15:26:53 -0500

    Add files via upload

---
+++ mi-primer-proyecto-node/Ejercicios/ejercicio-lista.js
@@ -0,0 +1,15 @@
+const librofav ={
+    titulo: "El perfume",
+    autor: "Patrick Suskind",
+    fecha: 2006
+}
+
+lista=[
+    "Arturo",
+    20,
+    false, 
+    new Date(2002,8,31),
+    librofav
+]
+
+console.log(lista)
