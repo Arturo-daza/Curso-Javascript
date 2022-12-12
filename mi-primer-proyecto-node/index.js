0769e575943619ed217c1c522ded53c8981dcc4b
Arturo-daza <118765168+Arturo-daza@users.noreply.github.com>
2022-12-12 10:45:42 -0500

    Add files via upload

---
+++ mi-primer-proyecto-node/index.js
@@ -0,0 +1,16 @@
+
+
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada")
+
+
+
+/*
+comentario
+multilinea
+*/
+
+
+
+// Este es el mensaje de despedida
+console.log("Adios")
